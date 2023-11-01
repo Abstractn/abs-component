dd8c9126d59f0ad74ce2496c2d9941c39b46773a
Abstractn <>
2023-11-02 00:38:19 +0100

    Added 'no-export' js file to dist

---
+++ dist/abs-component.nx.js
@@ -0,0 +1,111 @@
+class AbsComponentManager {
+    constructor(config) {
+        this.nodeAttributeSelector = config?.nodeAttributeSelector || AbsComponentManager.DEFAULT_NODE_ATTRIBUTE_SELECTOR;
+    }
+    static DEFAULT_NODE_ATTRIBUTE_SELECTOR = 'data-abs-component';
+    nodeAttributeSelector;
+    components = {};
+    componentsClassList = {};
+    registerComponent(templateReferenceName, scriptClass) {
+        this.componentsClassList[templateReferenceName] = scriptClass;
+    }
+    initComponents() {
+        try {
+            document.querySelectorAll(`[${this.nodeAttributeSelector}]`).forEach((componentNode) => {
+                const componentClassName = componentNode.getAttribute(this.nodeAttributeSelector);
+                if (componentClassName === null)
+                    throw [`[ABS] The following node's component data attribute value is null:`, componentNode];
+                if (this.componentsClassList[componentClassName] === undefined)
+                    throw `[ABS] Component initializer error: component "${componentClassName}" is not registered`;
+                if (this.components[componentClassName] === undefined) {
+                    this.components[componentClassName] = [];
+                }
+                const ComponentClass = this.componentsClassList[componentClassName];
+                const componentInstance = new ComponentClass(componentNode);
+                componentInstance.init();
+                this.components[componentClassName].push(componentInstance);
+            });
+            Object.keys(this.components).forEach(componentName => {
+                this.components[componentName].forEach(component => {
+                    if (component.ready) {
+                        component.ready();
+                    }
+                });
+            });
+        }
+        catch (error) {
+            if (Array.isArray(error)) {
+                console.error(...error);
+            }
+            else {
+                console.error(error);
+            }
+        }
+    }
+    initComponent(componentNode) {
+        try {
+            const componentClassName = componentNode.getAttribute(this.nodeAttributeSelector);
+            if (componentClassName === null)
+                throw [`[ABS] The following node's component data attribute value is null:`, componentNode];
+            if (this.componentsClassList[componentClassName] === undefined)
+                throw `[ABS] Component initializer error: component "${componentClassName}" is not registered`;
+            if (this.components[componentClassName] === undefined) {
+                this.components[componentClassName] = [];
+            }
+            const ComponentClass = this.componentsClassList[componentClassName];
+            const componentInstance = new ComponentClass(componentNode);
+            componentInstance.init();
+            this.components[componentClassName].push(componentInstance);
+            if (componentInstance.ready) {
+                componentInstance.ready();
+            }
+        }
+        catch (error) {
+            if (Array.isArray(error)) {
+                console.error(...error);
+            }
+            else {
+                console.error(error);
+            }
+        }
+    }
+    getComponentByNode(componentNode) {
+        let res = undefined;
+        const componentName = componentNode.getAttribute(this.nodeAttributeSelector);
+        if (this.components[componentName]) {
+            const componentSearchResult = this.components[componentName].find(componentInstance => componentInstance.node === componentNode);
+            if (componentSearchResult) {
+                res = componentSearchResult;
+            }
+        }
+        return res;
+    }
+    destroyComponent(component) {
+        Object.keys(this.components).forEach(componentName => {
+            const componentSearchResult = this.components[componentName].find(componentInstance => componentInstance === component);
+            if (componentSearchResult) {
+                const subComponentsNodeList = component.node.querySelectorAll(`[${this.nodeAttributeSelector}]`);
+                subComponentsNodeList.forEach(subComponentNode => {
+                    const subComponentReference = this.getComponentByNode(subComponentNode);
+                    if (subComponentReference)
+                        this.destroyComponent(subComponentReference);
+                });
+                if (component.destroy)
+                    component.destroy();
+                component.node.remove();
+                const componentIndex = this.components[componentName].indexOf(component);
+                this.components[componentName].splice(componentIndex, 1);
+            }
+        });
+    }
+    purgeComponentsList() {
+        Object.keys(this.components).forEach(componentName => {
+            this.components[componentName].forEach(component => {
+                const componentAttributeName = component.node.getAttribute(this.nodeAttributeSelector);
+                const isComponentAlive = Boolean(document.querySelector(`[${this.nodeAttributeSelector}="${componentAttributeName}"]`));
+                if (!isComponentAlive)
+                    this.destroyComponent(component);
+            });
+        });
+    }
+}
