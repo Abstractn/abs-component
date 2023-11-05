4d8b8d4d3440d9928d48a11da5c3c2a21085c156
Abstractn <>
2023-11-05 04:31:04 +0100

    Updated definitions file with autogenerated code

--- src/abs-component.d.ts
+++ src/abs-component.d.ts
@@ -1,22 +1,20 @@
-declare module 'AbsComponentManager' {
-  export interface AbsComponent {
+export declare namespace AbsComponent {
+  interface AbsComponent {
     init: () => void;
     ready?: () => void;
     destroy?: () => void;
     node: HTMLElement;
   }
-
-  export type AbsComponentList = Record<string, AbsComponent[]>;
-
-  export interface AbsComponentManagerConfig {
+  type AbsComponentList = Record<string, AbsComponent[]>;
+  interface AbsComponentManagerConfig {
     nodeAttributeSelector?: string;
   }
-
-  export class AbsComponentManager {
-    constructor (config?: AbsComponentManagerConfig);
-    static readonly DEFAULT_NODE_ATTRIBUTE_SELECTOR: 'data-abs-component';
+  class AbsComponentManager {
+    constructor(config?: AbsComponentManagerConfig);
+    static readonly DEFAULT_NODE_ATTRIBUTE_SELECTOR = "data-abs-component";
     readonly nodeAttributeSelector: string;
     readonly components: AbsComponentList;
+    private componentsClassList;
     registerComponent(templateReferenceName: string, scriptClass: new (node: HTMLElement) => AbsComponent): void;
     initComponents(): void;
     initComponent(componentNode: HTMLElement): void;
